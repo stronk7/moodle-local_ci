f8604c340572c4260edc9c76155556a1c3d1c4d1
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-07-02 19:46:33 +0200

    Improved count/last/best counters & diff generation

--- admin/ci/illegal_whitespace/illegal_whitespace.sh
+++ admin/ci/illegal_whitespace/illegal_whitespace.sh
@@ -5,15 +5,18 @@
 # dirs and files (egrep-like regexp) we are going to exclude from analysis
 exclude="/.git|/auth/cas/CAS|/lib/adodb|/lib/editor/tinymce/tiny_mce|/lib/typo3|/lib/yui|/lib/pear|/lib/spikephpcoverage|/lib/zend|/lib/overlib|/lib/minify|/lib/simpletestlib|/lib/swfobject|/lib/tcpdf|/repository/s3/S3.php|/repository/url/locallib.php|/search|/backup/bb/bb5.5_to_moodle.xsl|/backup/bb/bb6_to_moodle.xsl|/backup/cc/schemas|/lang/|/lib/alfresco|/lib/base32.php|/lib/bennu|/lib/csshover.htc|/lib/cookies.js|/lib/dragmath|/lib/excel|/lib/flowplayer|/lib/htmlpurifier|/lib/jabber/XMPP|/lib/markdown.php|/lib/simplepie|/lib/smarty|/lib/xhprof/xhprof_html|/lib/xhprof/xhprof_lib|/question/format/qti_two/templates|/webservice/amf/testclient/AMFTester.mxml|/webservice/amf/testclient/customValidators/JSONValidator.as"
 
-# file where results will be sent
-resultfile=$WORKSPACE/illegal_whitespace_$gitbranch.txt
-countsfile=$WORKSPACE/illegal_whitespace_$gitbranch.csv
+# files where results will be sent
+lastfile=$WORKSPACE/illegal_whitespace_last_execution_$gitbranch.txt
+correctfile=$WORKSPACE/illegal_whitespace_last_correct_$gitbranch.txt
+difffile=$WORKSPACE/illegal_whitespace_diff_$gitbranch.txt
+countfile=$WORKSPACE/illegal_whitespace_counters_$gitbranch.csv
+mincountfile=$WORKSPACE/illegal_whitespace_mincounter_$gitbranch.csv
 
 # Co to proper gitdir and gitpath
 cd $gitdir && git checkout $gitbranch && git reset --hard origin/$gitbranch
 
-# Search and send to $resultfile
-echo -n > "$resultfile"
+# Search and send to $lastfile
+echo -n > "$lastfile"
 for i in `find . -type f`
 do
     if [[ $i =~ $exclude ]]
@@ -23,25 +26,66 @@ do
     content=`grep -PIn '^[ \t]+$|^ *\t *.+$|^.*[ \t]+$' $i`
     if [ ! -z "$content" ]
     then
-        echo "## $i ##" >> "$resultfile"
-        echo "$content" >> "$resultfile"
+        echo "## $i ##" >> "$lastfile"
+        echo "$content" >> "$lastfile"
     fi
 done
 
-# Count and send to countsfile
-count=`cat "$resultfile" | wc -l`
-echo "$BUILD_NUMBER	$BUILD_ID	$count" >> "$countsfile"
-
-# Compare 2 last executions to decide return status
-count=0
-for i in `tail -2 "$countsfile" | cut -s -f3`
-do
-    prevcount=$count
-    count=$i
-done
-# Number of incorrect whitespace has grown
-if (($prevcount < $count))
+# Get the count from the previous execution
+prevcount=999999
+if [[ -f "$countfile" ]]
 then
-    exit 1
+    prevcount=`tail -1 "$countfile" | cut -s -f3`
 fi
-exit 0
+
+# Count and send to countfile
+count=`cat "$lastfile" | wc -l`
+echo "$BUILD_NUMBER	$BUILD_ID	$count" >> "$countfile"
+
+# Get best count ever or create it
+bestcount=999999
+if [[ ! -f "$mincountfile" ]]
+then
+    # Create the file (first run) with current counter
+    echo "$BUILD_NUMBER	$BUILD_ID	$bestcount" > "$mincountfile"
+else
+    # Read the best counter
+    bestcount=`tail -1 "$mincountfile" | cut -s -f3`
+fi
+
+echo " current count: $count"
+echo "previous count: $prevcount"
+echo "    best count: $bestcount"
+
+# Exit status = 1 by default
+status=1
+
+# Compare counter with previous counter
+if (($count > $prevcount))
+then
+    # The counter has grown, worse results: make difffile
+    echo "worse results than previous counter"
+    diff "$correctfile" "$lastfile" > "$difffile"
+else
+    # The counter is same or better, lets compare with best counter
+    echo "same/better results than previous counter"
+    if (($count < $bestcount))
+    then
+        # Best ever, save current counter as best, grab correctfile and delete diff
+        echo "got best results ever, yay!"
+        echo "$BUILD_NUMBER	$BUILD_ID	$count" > "$mincountfile"
+        cp "$lastfile" "$correctfile"
+        rm -fr "$difffile"
+        status=0
+    elif (($count == $bestcount))
+    then
+        # Continue in best ever
+        echo "continue in best results ever"
+        rm -fr "$difffile"
+        status=0
+    else
+        # No best ever yet
+        echo "still no back to best ever"
+    fi
+fi
+exit $status
