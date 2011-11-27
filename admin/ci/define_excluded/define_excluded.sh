7ca3027066777d67f3ebd0330e5b50b702f47cd7
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-11-27 03:30:00 +0100

    Common excluded paths in various tests

---
+++ admin/ci/define_excluded/define_excluded.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Define directories usually excluded by various CI tools
+excluded=".git
+auth/cas/CAS
+backup/cc/schemas
+backup/cc/schemas11
+lib/adodb
+lib/editor/tinymce/tiny_mce
+lib/typo3
+lib/yui
+lib/pear
+lib/spikephpcoverage
+lib/zend
+lib/overlib
+lib/minify
+lib/simpletestlib
+lib/swfobject
+lib/tcpdf
+repository/s3/S3.php
+repository/url/locallib.php
+search
+backup/bb/bb5.5_to_moodle.xsl
+backup/bb/bb6_to_moodle.xsl
+backup/cc/schemas
+lang/
+lib/alfresco
+lib/base32.php
+lib/bennu
+lib/csshover.htc
+lib/cookies.js
+lib/dragmath
+lib/excel
+lib/flowplayer
+lib/htmlpurifier
+lib/jabber/XMPP
+lib/markdown.php
+lib/simplepie
+lib/smarty
+lib/xhprof/xhprof_html
+lib/xhprof/xhprof_lib
+mod/lti/OAuthBody.php
+question/format/qti_two/templates
+theme/mymobile/javascript
+theme/mymobile/style/jmobilerc2.css
+webservice/amf/testclient/AMFTester.mxml
+webservice/amf/testclient/customValidators/JSONValidator.as"
+
+# Exclude syntax for grep commands (egrep-like regexp)
+excluded_grep=""
+for i in ${excluded}
+do
+    excluded_grep="${excluded_grep}|/${i}"
+done
+excluded_grep=${excluded_grep//|\/\.git/\/.git}
+
+# Exclude syntax for phpcpd (list of exclude parameters)
+excluded_list=""
+for i in ${excluded}
+do
+    excluded_list="${excluded_list} --exclude ${i}"
+done
+echo ${excluded_list}
