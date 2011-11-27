58e2ed5b7da96a831cd93952a3f9f46f9506dd9a
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-11-27 19:53:22 +0100

    mess detector job

---
+++ admin/ci/project_mess_detector/project_mess_detector.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# $gitdir: Directory containing git repo
+# $gitbranch: Branch we are going to check
+# file where results will be sent
+resultfile=${WORKSPACE}/project_mess_detector.xml
+
+# calculate some variables
+mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# list of excluded dirs
+. ${mydir}/../define_excluded/define_excluded.sh
+
+# checkout pristine copy of the configure branch
+cd ${gitdir} && git checkout ${gitbranch} && git reset --hard origin/${gitbranch}
+
+# Run phpmd against the whole codebase
+/opt/local/bin/php ${mydir}/project_mess_detector.php ${gitdir} xml codesize,unusedcode,design --exclude ${excluded_comma} --reportfile "${resultfile}"
+# Always return ok
+exit 0
