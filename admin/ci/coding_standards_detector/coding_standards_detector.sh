07b2ed42dbdf60f104fe5edf880a30bb300acb22
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-11-28 16:56:45 +0100

    Code checker running against complete codebase

---
+++ admin/ci/coding_standards_detector/coding_standards_detector.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# $gitdir: Directory containing git repo
+# $csdir: Directory containing moodle phpcs standard definition
+# $gitbranch: Branch we are going to check
+# $extraoptions: Extra options to pass to phpcs
+# $extraignore: Extra ignore dirs
+
+# file where results will be sent
+resultfile=${WORKSPACE}/coding_standards_detector.xml
+
+# calculate some variables
+mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# list of excluded dirs
+. ${mydir}/../define_excluded/define_excluded.sh
+
+# checkout pristine copy of the configure branch
+cd ${gitdir} && git checkout ${gitbranch} && git reset --hard origin/${gitbranch}
+
+# Run phpcs against the whole codebase
+/opt/local/bin/php ${mydir}/coding_standards_detector.php --report=checkstyle --report-file="${resultfile}" --standard="${csdir}" --ignore="${excluded_comma_wildchars}${extraignore}" ${extraoptions} $gitdir
+# Always return ok
+exit 0
