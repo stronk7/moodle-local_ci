6cd37bb9501617e947edf468bbcef1c8912a58c1
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-12-13 22:05:29 +0100

    Launch the copy/paste globally, no by modules

--- admin/ci/copy_paste_detector/copy_paste_detector.sh
+++ admin/ci/copy_paste_detector/copy_paste_detector.sh
@@ -11,21 +11,10 @@ mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # checkout pristine copy of the configure branch
 cd ${WORKSPACE} && git checkout ${gitbranch} && git fetch && git reset --hard origin/${gitbranch}
 
-# Create all the ant build files to specify the modules
-/opt/local/bin/php ${mydir}/../generate_component_ant_files/generate_component_ant_files.php --basedir="${WORKSPACE}"
-
-# Look for all the build.xml files, running phpcpd for them
-buildxml="$( find "${WORKSPACE}" -name build.xml | sed 's/\/build.xml//g' | sort -r)"
-for dir in ${buildxml}
-    do
-        echo "processing ${dir}"
-        cd ${dir}
-        /opt/local/bin/php ${mydir}/copy_paste_detector.php ${excluded_list} --quiet --log-pmd "${dir}/${resultfilename}" .
-        excluded_list="${excluded_list} --exclude ${dir}"
-    done
-
-# checkout pristine copy of the configure branch
-cd ${WORKSPACE} && git checkout ${gitbranch} && git fetch && git reset --hard origin/${gitbranch}
+# Process the whole workspace
+echo "processing ${WORKSPACE}"
+echo "with excluded ${excluded_list}"
+/opt/local/bin/php ${mydir}/copy_paste_detector.php ${excluded_list} --quiet --log-pmd "${WORKSPACE}/${resultfilename}" .
 
 # Always return ok
 exit 0
