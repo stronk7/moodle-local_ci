d05264f988d04b249d4eff381e56b9bc0249d20b
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-12-15 13:31:52 +0100

    1st cut of simpletest runner

---
+++ admin/ci/run_simpletests/lib.php
@@ -0,0 +1,48 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Library of classes/functions for run_simpletests
+ *
+ * @package    core
+ * @subpackage ci
+ * @copyright  2011 Eloy Lafuente (http://stronk7.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class cli_text_reporter extends TextReporter {
+
+    function paintNotice($message) { }
+}
+
+class cli_html_reporter extends ExHtmlReporter {
+
+}
+
+class cli_xml_reporter extends XMLReporter {
+
+}
+
+class cli_xunit_reporter extends XMLReporter {
+
+}
+
+class cli_group_test extends AutoGroupTest {
+
+    public $ignorefolders = array();
+
+}
