3833d813df50ade0ba071e1868e7e3fea07ca058
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-12-16 01:37:30 +0100

    shell script 1st cut

---
+++ admin/ci/run_simpletests/run_simpletests.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# $gitdir: Directory containing git repo
+# $gitbranch: Branch we are going to install the DB
+# $dblibrary: Type of library (native, pdo...)
+# $dbtype: Name of the driver (mysqli...)
+# $dbhost: DB host
+# $dbuser: DB user
+# $dbpass: DB password
+# $testpath: Path (gitdir based) we want tests to run
+
+# file where results will be sent
+resultfile=${WORKSPACE}/run_simpletests.xml
+
+# calculate some variables
+mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+installdb=ci_installed_${BUILD_NUMBER}_${EXECUTOR_NUMBER}
+datadir=/tmp/ci_dataroot_${BUILD_NUMBER}_${EXECUTOR_NUMBER}
+dbprefixinstall="cit_"
+
+# Going to install the $gitbranch database
+# Create the database
+# TODO: Based on $dbtype, execute different DB creation commands
+mysql --user=$dbuser --password=$dbpass --host=$dbhost --execute="CREATE DATABASE $installdb CHARACTER SET utf8 COLLATE utf8_bin"
+# Do the moodle install
+cd $gitdir && git checkout $gitbranch && git reset --hard origin/$gitbranch
+rm -fr config.php
+/opt/local/bin/php admin/cli/install.php --non-interactive --allow-unstable --agree-license --wwwroot="http://localhost" --dataroot="$datadir" --dbtype=$dbtype --dbhost=$dbhost --dbname=$installdb --dbuser=$dbuser1 --dbpass=$dbpass --prefix=$dbprefixinstall --fullname=$installdb --shortname=$installdb --adminuser=$dbuser --adminpass=$dbpass
+
+# Copy the run utility to the $gitdir
+mkdir -p $gitdir/admin/ci/run_simpletests
+cp $mydir/run_simpletests.php $gitdir/admin/ci/run_simpletests/
+
+# Execute the simpletest utility
+echo /opt/local/bin/php ${gitdir}/admin/ci/run_simpletests/run_simpletests.php --format=xunit --path=${testpath} > "${resultfile}"
+/opt/local/bin/php ${gitdir}/admin/ci/run_simpletests/run_simpletests.php --format=xunit --path=${testpath} > "${resultfile}"
+exitstatus=${PIPESTATUS[0]}
+
+# Drop the databases and delete files
+# TODO: Based on $dbtype, execute different DB deletion commands
+mysqladmin --user=$dbuser --password=$dbpass --host=$dbhost --default-character-set=utf8 --force drop $installdb
+rm -fr config.php
+rm -fr $gitdir/admin/ci
+rm -fr $datadir
+
+# If arrived here, return the exitstatus of the php execution
+exit $exitstatus
